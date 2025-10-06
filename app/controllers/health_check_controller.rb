39144308963ea0f6df5cbde50558bfbee902b744
admin <admin@nightonly.com>
2025-10-06 18:26:59 +0900

    feat origin ヘルスチェックの対象にDBを追加

--- app/controllers/health_check_controller.rb
+++ app/controllers/health_check_controller.rb
@@ -1,6 +1,10 @@
 class HealthCheckController < ApplicationController
-  layout false
-
   # GET /health_check ヘルスチェック
-  def index; end
+  def index
+    ActiveRecord::Base.connection.execute('SELECT 1') if %w[all db].include?(params[:target])
+
+    render plain: 'OK'
+  rescue ActiveRecord::ActiveRecordError => e
+    render plain: e.class, status: :internal_server_error
+  end
 end
--- spec/requests/health_check_spec.rb
+++ spec/requests/health_check_spec.rb
@@ -1,12 +1,88 @@
 require 'rails_helper'
 
 RSpec.describe 'HealthChecks', type: :request do
+  # GET /health_check ヘルスチェック
+  # テストパターン
+  #   正常, DBエラー
+  #   パラメータがない, 対象: 存在しない, db, all
   describe 'GET #index' do
-    subject { get health_check_path }
+    subject { get health_check_path, params: }
 
-    it 'HTTPステータスが200。OKが返却される' do
-      is_expected.to eq(200)
-      expect(response.body).to eq('OK')
+    # テスト内容
+    shared_examples 'OK' do
+      it 'HTTPステータスが200。OKが返却される' do
+        is_expected.to eq(200)
+        expect(response.body).to eq('OK')
+
+        expect(ActiveRecord::Base.connection).not_to have_received(:execute)
+      end
+    end
+
+    shared_examples 'OK(DB)' do
+      it 'HTTPステータスが200。OKが返却される' do
+        is_expected.to eq(200)
+        expect(response.body).to eq('OK')
+
+        expect(ActiveRecord::Base.connection).to have_received(:execute).once.with('SELECT 1')
+      end
+    end
+    shared_examples 'NG(DB)' do
+      it 'HTTPステータスが500。エラーメッセージが返却される' do
+        is_expected.to eq(500)
+        expect(response.body).to eq('ActiveRecord::DatabaseConnectionError')
+
+        expect(ActiveRecord::Base.connection).to have_received(:execute).once.with('SELECT 1')
+      end
+    end
+
+    # テストケース
+    context '正常' do
+      before { allow(ActiveRecord::Base.connection).to receive(:execute).with('SELECT 1').and_return(nil) }
+
+      context 'パラメータがない' do
+        let(:params) { nil }
+
+        it_behaves_like 'OK'
+      end
+      context '対象が存在しない' do
+        let(:params) { { target: 'not' } }
+
+        it_behaves_like 'OK'
+      end
+      context '対象がdb' do
+        let(:params) { { target: 'db' } }
+
+        it_behaves_like 'OK(DB)'
+      end
+      context '対象がall' do
+        let(:params) { { target: 'all' } }
+
+        it_behaves_like 'OK(DB)'
+      end
+    end
+    context 'DBエラー' do
+      before { allow(ActiveRecord::Base.connection).to receive(:execute).with('SELECT 1').and_raise(ActiveRecord::DatabaseConnectionError) }
+
+      context 'パラメータがない' do
+        let(:params) { nil }
+
+        it_behaves_like 'OK'
+      end
+      context '対象が存在しない' do
+        let(:params) { { target: 'not' } }
+
+        it_behaves_like 'OK'
+      end
+      context '対象がdb' do
+        let(:params) { { target: 'db' } }
+
+        it_behaves_like 'NG(DB)'
+      end
+      context '対象がall' do
+        let(:params) { { target: 'all' } }
+
+        it_behaves_like 'NG(DB)'
+      end
     end
   end
 end
